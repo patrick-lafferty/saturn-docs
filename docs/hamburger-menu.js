e050b9d7191e3121c8843fd1110fff4c25b3703f
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-05-08 17:18:14 -0400

    Rearrange hamburger menu to look better on mobile devices

--- docs/hamburger-menu.js
+++ docs/hamburger-menu.js
@@ -1,16 +1,18 @@
 (function () {
-    var hamburger = document.querySelector(".hamburger");
+    var hamburger = document.querySelector(".hamburgerLogo");
     
     if (hamburger.style.display != 'none') {
         hamburger.addEventListener('click', function () {
-        var menu = document.querySelector(".menu");
-        
-        if (menu.style.display == 'flex') {
-            menu.style.display = 'none';
-        }
-        else {
-            menu.style.display = 'flex';
-        }
+        var menu = document.querySelectorAll(".menu li");
+
+        menu.forEach(function(item) {
+            if (item.style.display == 'block') {
+                item.style.display = 'none';
+            }
+            else {
+                item.style.display = 'block';
+            }
+        });
 
     }, false);
     }
