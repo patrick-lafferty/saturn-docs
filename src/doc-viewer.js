71c87988cabf6074ecbbc2aa39ab8712ca67aac3
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-05-11 16:10:03 -0400

    Start working on browser
    Adds support for files in the left side list, and updating the viewer's
    function list when a new file is selected.

--- src/doc-viewer.js
+++ src/doc-viewer.js
@@ -28,17 +28,205 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import React, {Component} from 'react';
 import style from './saturn.css';
 
+class Function extends Component {
+    constructor(props) {
+        super(props);
+
+        this.signature = props.signature;
+        this.description = props.description;
+    }
+
+    render() {
+        return (
+            <li>
+                <section className={`code`}>
+                    <a className={`codeLink darkBlue`}>
+                        {this.signature}
+                    </a>
+
+                    <p>{this.description}</p>
+                </section>
+            </li>
+        );
+    }
+}
+
+class File extends Component {
+    
+    render() {
+        return (
+            <ul>
+                {this.props.functions.map((f) => 
+            <Function key={f.signature} signature={f.signature} description={f.description} />)}
+            </ul>
+        );
+    }
+}
+
+/*
+Can be a directory, file, or a class
+*/
+class ListItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.item = props.item;
+    }
+
+    render() {
+        return (
+            <div className={`searchResult`}>
+                <span className={`resultType ${this.item.classType}`}>
+                    {this.item.type}</span> 
+                    {this.item.name}
+            </div>
+        );
+    }
+
+}
+
+class Renderable extends Component {
+
+    render() {
+
+        switch (this.props.item.type) {
+            case 'file': {
+                return <File functions={this.props.item.contents.functions} />;
+                break;
+            }
+        }
+
+    }
+}
+
 class DocumentationViewer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        let items = [
+            {
+                type: "file",
+                classType: "fileType",
+                name: "vostok.cpp",
+
+                contents: {
+                    functions: [
+                        {
+                            signature: "void func(int arg)",
+                            description: "describing the description"
+                        },
+                        {
+                            signature: "int foo(char bar)",
+                            description: "another function"
+                        }
+                    ]
+                }
+            },
+            {
+                type: "file",
+                classType: "fileType",
+                name: "kernel.cpp",
+
+                contents: {
+                    functions: [
+                        {
+                            signature: "int kernel_main(int arg)",
+                            description: "the main entry point"
+                        }
+                        
+                    ]
+                }
+            }, 
+            {
+                type: "file",
+                classType: "fileType",
+                name: "ipc.cpp",
+
+                contents: {
+                    functions: [
+                        {
+                            signature: "int somethingA(int arg)",
+                            description: "textA"
+                        },
+                        {
+                            signature: "int somethingB(int arg)",
+                            description: "textB"
+                        },
+                        {
+                            signature: "int somethingC(int arg)",
+                            description: "textC"
+                        }
+                        
+                    ]
+                }
+            }
+        ];
+
+        this.renderables = items.map((item) => <Renderable item={item} />);
+        this.state = {rend: this.renderables[0], index: 0};
+
+        this.handleClick = function (index, e) {
+
+            this.setState({
+                rend: this.renderables[index],
+                index: index
+            });
+        };
+
+        this.items = items.map((item, index) => 
+                <ListItem item={item} />);
+    }
+
     render() {
         return(
             <div className={style.blueBackground}>
-                    <main className={style.main}>
-                        <p>
-                            Documentation will be a ReactJS source code/documentation browser,
-                            it will look like the template below. 
-                        </p>
-                    </main>
+                <main>
+                    <div className={`browser`}>
+                        <div className={`browserMenu darkBlue`}>
+                            <div className={`tabBar`}>
+
+                                <div className={`tab lightBlue`}>Browse</div>
+                                <div className={`tab`}>Search</div>
+
+                            </div>
+                        </div>
+
+                        <div className={`browserNavigation lightBlue`}>
+                            <div className={`navigation`}>
+
+                                <div className={`breadcrumb`}>
+                                    <a className={`breadcrumbLink`} >/</a>
+                                </div>
+
+                                <div className={`breadcrumb`}>
+                                    <a className={`breadcrumbLink`} >services</a>
+                                </div>
+
+                                <div className={`breadcrumb darkBlue`}>
+                                    <a className={`breadcrumbLink darkBlue`} >apollo</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <ul className={`list lightBlue`}>
+                            {this.items.map((item, index) => 
+                                <li className={index == this.state.index ? `darkBlue` : `lightBlue`} 
+                                    key={index.toString()} 
+                                    onClick={this.handleClick.bind(this, index)}>
+
+                                    {item}
+                                </li>
+                            )}
+                        </ul>
+
+                        <div className={`viewer darkBlue`}>
+                            {this.state.rend}
+                        </div>
+
                     </div>
+                </main>
+            </div>
         );
     }
 }
