49f1901bf41d8b0578ad214d1ef53e13a78e7367
Patrick Lafferty <patrick_lafferty@outlook.com>
2018-05-15 01:42:07 -0400

    Add methods to jsonify classes/functions

--- src/build-docs.py
+++ src/build-docs.py
@@ -1,5 +1,6 @@
 import clang.cindex
 import sys
+import json
 from clang.cindex import *
 
 def search(cursor):
@@ -17,6 +18,9 @@ class Constructor:
     def show(self, indent):
         print " " * indent, self.node.spelling
 
+def cleanComment(comment):
+    return comment.replace('\r\n', '').replace('/*', '').replace('*/', '').replace('\'', '\\\'')
+
 class Method:
 
     def __init__(self, node):
@@ -25,6 +29,7 @@ class Method:
         self.returnType = None
         self.returnQualifiers = []
         self.node = node
+        self.comment = cleanComment(node.raw_comment) if node.raw_comment else ""
 
         qualifiers = []
 
@@ -41,8 +46,17 @@ class Method:
             elif child.kind == CursorKind.TEMPLATE_REF:
                 qualifiers.append(child)
             else:
-                print "unhandled method child", child.kind, child.spelling, node.spelling
+                pass
+                #KEEP
+                #print "unhandled method child", child.kind, child.spelling, node.spelling
 
+    def toJson(self):
+        
+        params = ", ".join([x.type.spelling for x in self.parameters])
+        signature = "{0} {1}({2})".format(self.node.result_type.spelling, self.name, params)
+
+        template = "{{signature: '{0}', description: '{1}'}}"
+        return template.format(signature, self.comment)
 
     def show(self, indent):
 
@@ -65,6 +79,8 @@ class Class:
         self.publicFields = {}
         self.protectedFields = {}
         self.privateFields = {}
+        self.comment = cleanComment(node.raw_comment) if node.raw_comment else ""
+        self.filename = node.location.file.name
 
         currentAccess = clang.cindex.AccessSpecifier.PUBLIC if isStruct else clang.cindex.AccessSpecifier.PRIVATE
 
@@ -91,6 +107,19 @@ class Class:
                 pass
                 #KEEP THIS
                 #print "unhandled class child ", child.kind, child.spelling, node.spelling
+
+    def isEmpty(self):
+        return not (self.constructors or self.publicMethods or self.publicFields)
+
+    def toJson(self):
+
+        publicMethodJsons = []
+
+        for x in self.publicMethods:
+            publicMethodJsons.append(x.toJson())
+
+        template = "{{name: '{0}', classComment: '{1}', publicMethods: [{2}]}}"
+        return template.format(self.name, self.comment, ", ".join(publicMethodJsons))
 
     def show(self, indent):
 
@@ -130,6 +159,7 @@ class Namespace:
     def __init__(self, node):
         self.node = node
         self.classes = []
+        self.functions = []
         self.structs = []
         self.namespaces = []
         self.nodes = []
@@ -146,8 +176,10 @@ class Namespace:
                 self.structs.append(Class(child, True))
             elif child.kind == CursorKind.NAMESPACE:
                 self.namespaces.append(Namespace(child))
+            elif child.kind == CursorKind.FUNCTION_DECL:
+                self.functions.append(Method(child))
             else:
-                #print "unhandled child type ", child.kind
+                print "unhandled child type ", child.kind
                 pass
 
 
@@ -167,11 +199,24 @@ class Namespace:
         for node in self.nodes:
             print " " * indent, node.spelling
 
+    def collect(self, classes, functions):
+
+        if "saturn/src" in self.node.location.file.name:
+            for x in self.classes:
+                if not x.isEmpty():
+                    classes.append(x.toJson())
+
+            for x in self.functions:
+                functions.append(x.toJson())
+
+        for x in self.namespaces:
+            x.collect(classes, functions)
+
 toplevelNamespaces = {}
 
 def recurse(node, indent = 0):
 
-    if node.kind == CursorKind.NAMESPACE and "saturn" in node.location.file.name:
+    if node.kind == CursorKind.NAMESPACE and "saturn/src" in node.location.file.name:
 
         if node.spelling in toplevelNamespaces:
             toplevelNamespaces[node.spelling].append(node)
@@ -197,5 +242,17 @@ for x in translationUnit.diagnostics:
     print x
 
 recurse(translationUnit.cursor)
+classes = []
+functions = []
+
+for name, ns in toplevelNamespaces.iteritems():
+    ns.collect(classes, functions)
+
+classesJson = ", ".join(classes)
+functionsJson = ", ".join(functions)
+
+print classesJson
+print ""
+print functionsJson
 
 #toplevelNamespaces['Apollo'].show(0)
